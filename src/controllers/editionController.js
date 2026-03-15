953448e63f8219162988ae743f2abd0a4d4ef1d7
Sam Pons de la Fuente <sampf2006@gmail.com>
2026-03-15 22:38:20 +0100

    add update, delete editions

--- src/controllers/editionController.js
+++ src/controllers/editionController.js
@@ -1,6 +1,42 @@
 import { Edition } from "../models/Edition.js";
 
 export const editionController = {
+  deleteEdition: async (req, res) => {
+    try {
+      const id = req.params.id;
+      console.log(id)
+      const result = await Edition.deleteEdition(id);
+      
+      if (result.affectedRows !== 0) {
+        res.status(200).json({"ok": true, "message": "Deleted correctly"})
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  },
+  updateEdition: async (req, res) => {
+    const id = req.params.id
+    const payload = req.body
+
+    const startDate = new Date(payload.selectedRange.from).toISOString().slice(0, 19).replace('T', ' ');
+    const endDate = new Date(payload.selectedRange.to).toISOString().slice(0, 19).replace('T', ' ');
+    const data = {
+      name: payload.editionName,
+      startDate,
+      endDate,
+      isOpen: payload.isOpen
+    }
+    try {
+      const result = await Edition.updateEdition(id, data)
+
+      if (result.changedRows !== 0) {
+        res.status(200).json({"ok": true, "message": "Updated correctly"})       
+      }
+    } catch (err) {
+      console.log(err)
+    }
+    
+  },
   getAllEditions: async (req, res) => {
     try {
       const editions = await Edition.getAllEditions();
